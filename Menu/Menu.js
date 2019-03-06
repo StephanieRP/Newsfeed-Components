ab6fc678c8e8dec6f35c675218363ee46eee985d
Stephanie Patton <31425878+StephanieRP@users.noreply.github.com>
2019-03-06 17:38:54 -0600

    added menu button animation

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -3,9 +3,23 @@ const toggleMenu = () => {
   menu.classList.toggle("menu--open");
 };
 
+const moveRight = () => {
+  menuButton.classList.add("menu-animate");
+};
+
+const moveLeft = () => {
+  menuButton.classList.remove("menu-animate");
+};
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector(".menu");
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
+const header = document.querySelector(".header");
+console.log(header);
+
+//menu animation
+
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener("click", toggleMenu);
+header.addEventListener("mouseover", moveRight);
+header.addEventListener("mouseleave", moveLeft);
