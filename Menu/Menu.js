eccd8f7c9e3eaf48045729847433e848ea9b9faf
Stephanie Patton <31425878+StephanieRP@users.noreply.github.com>
2019-03-06 17:55:48 -0600

    added transition to menu button

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -10,16 +10,21 @@ const moveRight = () => {
 const moveLeft = () => {
   menuButton.classList.remove("menu-animate");
 };
+
+const closeMenu = () => {
+  if (menu.classList.contains("menu--open")) {
+    menu.classList.remove("menu--open");
+    menuButton.classList.remove("menu-animate");
+  }
+};
+
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector(".menu");
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector(".menu-button");
 const header = document.querySelector(".header");
-console.log(header);
-
-//menu animation
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
 menuButton.addEventListener("click", toggleMenu);
 header.addEventListener("mouseover", moveRight);
-header.addEventListener("mouseleave", moveLeft);
+window.addEventListener("dblclick", closeMenu);
